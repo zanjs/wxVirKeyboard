47d56e8d8a64d80c292d60b742150d25f08757a0
yun <153967808@qq.com>
2017-06-20 10:25:29 +0800

    优化体验，优化性能

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # wxVirKeyboard
 # 微信小程序中的虚拟键盘，此处专为车牌号打造的。
-![demo.png](https://ooo.0o0.ooo/2017/06/09/593a49929768d.png)
+![QQ截图20170620102242.png](https://ooo.0o0.ooo/2017/06/20/59488713970f5.png)
+![QQ截图20170620102252.png](https://ooo.0o0.ooo/2017/06/20/59488713b9f4b.png)
--- pages/keyboard/keyboard.js
+++ pages/keyboard/keyboard.js
@@ -7,18 +7,23 @@ Page({
    * keyboard2:第二页键盘，显示数字和大写字母
    */
   data: {
-    isKeyboard: false,
+    isKeyboard: false,//是否显示键盘
     specialBtn: false,
-    tapNum: false,
+    tapNum: false,//数字键盘是否可以点击
+    parkingData:true,//是否展示剩余车位按钮
+    isFocus:false,//输入框聚焦
+    phoneNumber: '8888888',
     keyboardNumber: '1234567890',
     keyboardAlph: 'QWERTYUIOPASDFGHJKLZXCVBNM',
     keyboard1: '京津沪冀豫云辽黑湘皖鲁新苏浙赣鄂桂甘晋蒙陕吉闽贵粤川青藏琼宁渝',
     keyboard2: '',
     keyboard2For: ['删除', '完成'],
     keyboardValue: '',
-    textValue: '',
     textArr: [],
-    animationData: {},
+    textValue: '',
+    placeholder:'点此输入车牌',
+    animationData: {},//动画事件
+    warnMessage:'提示：请确保您填写车牌号的正确性，以免后续误交费给您造成不必要的麻烦。',
   },
 
   /**
@@ -34,19 +39,31 @@ Page({
     })
   },
   /**
+   * 输入框显示键盘状态
+   */
+  showKeyboard:function(){
+    var self =this;
+      self.setData({
+        isFocus: true,
+        isKeyboard: true,
+      })
+  },
+  /**
    * 输入框聚焦触发，显示键盘
    */
-  bindTextAreaFocus: function () {
+  bindFocus: function () {
     var self = this;
-    if (self.data.isKeyboard){
+    if (self.data.isKeyboard) {
       //说明键盘是显示的，再次点击要隐藏键盘
       self.setData({
-        isKeyboard: false
+        isKeyboard: false,
+        isFocus: false,
       })
-    }else{
+    } else {
       //说明键盘是隐藏的，再次点击显示键盘
       self.setData({
-        isKeyboard: true
+        isFocus: true,
+        isKeyboard: true,
       })
     }
   },
@@ -92,7 +109,7 @@ Page({
         this.specialBtn = false;
         this.tapNum = false;
         this.keyboardValue = self.data.keyboard1;
-      } else if (self.data.textArr.length == 1){
+      } else if (self.data.textArr.length == 1) {
         //只能输入字母
         this.tapNum = false;
         this.specialBtn = true;
@@ -113,42 +130,65 @@ Page({
       //说明是完成事件
       if (self.data.textArr.length < 7) {
         wx.showToast({
-            title: '请输入正确的车牌号',
-            icon: 'success',
-            image:'../../images/demo-photo01.png',
-            duration: 2000
-          })
-      }else{
+          title: '请输入正确的车牌号',
+          icon: 'success',
+          mask: true,
+          image: '../../images/icon_error.png',
+          duration: 2000
+        })
+      } else {
         self.qrydata(self.data.textValue);
       }
     }
+  },
+  /**
+   * 点击查询剩余车位按钮
+   */
+  qryParking:function(){
+    var self = this;
+    wx.navigateTo({
+      url: '../parking/parking'
+    })
   },
   /**
    * 请求服务器
    */
   qrydata: function (textValue) {
     var self = this;
-    var msg = "未查询到车牌号为" + textValue+"车辆的相关信息"
-    wx.showModal({
-      title: msg,
-      content: '请确认您输入的车牌号无误，且车辆停放在西北服务区停车场！',
-      showCancel:false,
-      success: function (res) {
-        if (res.confirm) {
-          console.log('用户点击确定')
-        } else if (res.cancel) {
-          console.log('用户点击取消')
+    
+      // wx.request({
+      //   url: '',
+      //   data: {
+      //     textValue: self.data.textValue
+      //   },
+      //   header: {
+      //     'content-type': 'application/json'
+      //   },
+      //   success: function (res) {
+      //     console.log(res.data)
+      var msg = "未查询到车牌号为" + textValue + "车辆的相关信息"
+      wx.showModal({
+        title: msg,
+        content: '请确认您输入的车牌号无误，且车辆停放在西北服务区停车场！',
+        showCancel: false,
+        success: function (res) {
+          if (res.confirm) {
+            console.log('用户点击确定')
+          } else if (res.cancel) {
+            console.log('用户点击取消')
+          }
         }
-      }
-    })
+      })
+      //   }
+      // })
   },
   /**
    * 拨打电话
    */
-  phoneCall:function(){
-    var self =this;
+  phoneCall: function () {
+    var self = this;
     wx.makePhoneCall({
-      phoneNumber: '4000000' //仅为示例，并非真实的电话号码
+      phoneNumber: self.data.phoneNumber
     })
   },
   /**
@@ -164,26 +204,21 @@ Page({
   onShow: function () {
     var self = this;
     // 输入提示竖线动画
-    var animation = wx.createAnimation({
-      duration: 200,
-      timingFunction: 'easy',
-    })
-    self.animation = animation
-    self.setData({
-      animationData: animation.export()
-    })
-    var interver1 = setInterval(function () {
-      animation.opacity(1).step()
-      self.setData({
-        animationData: animation.export()
-      })
-    }.bind(self), 600)
-    var interver0 = setInterval(function () {
-      animation.opacity(0).step()
-      self.setData({
-        animationData: animation.export()
-      })
-    }.bind(self), 1200)
+    // var animation = wx.createAnimation({
+    //   duration: 200,
+    //   timingFunction: 'easy',
+    // })
+    // self.animation = animation
+    // self.setData({
+    //   animationData: animation.export()
+    // })
+    // var interver0 = setInterval(function () {
+    //   animation.opacity(0).step()
+    //   animation.opacity(1).step({ duration: 1000})
+    //   self.setData({
+    //     animationData: animation.export()
+    //   })
+    // }.bind(self), 3000)
   },
 
   /**
@@ -211,7 +246,7 @@ Page({
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-
+    wx.stopPullDownRefresh()
   },
 
   /**
